fe7344d4fd9ea42c0d0bdbb8f619b6d3fc0bc36a
andrewsfisk <andrewsfisk@gmail.com>
2023-05-14 22:39:47 +1000

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
-import NavBar from "./components/navbar/NavBar";
+import React from "react";
+import { Routes, Route, NavLink as Link } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Bookkeeping from "./pages/Bookkeeping";
+import Mentoring from "./pages/Mentoring";
+import Contact from "./pages/Contact";
+import logo from "./logo.png";
+import image from "./image.png";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -7,7 +15,58 @@ import "@fontsource/roboto/700.css";
 import "@fontsource/nunito";
 
 function App() {
-  return <NavBar />;
+  return (
+    <div className="">
+      <div>
+        <nav>
+          <img
+            className="logo"
+            width="150px"
+            height="auto"
+            alt="logo"
+            src={logo}
+          ></img>
+          <ul>
+            <li>
+              <Link to="/" activeClassName="active">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/Bookkeeping" activeClassName="active">
+                Bookkeeping
+              </Link>
+            </li>
+            <li>
+              <Link to="/Mentoring" activeClassName="active">
+                Hospitality Mentoring
+              </Link>
+            </li>
+            <li>
+              <Link to="/Contact" activeClassName="active">
+                Contact
+              </Link>
+            </li>
+          </ul>
+          <img
+            className="navimg"
+            alt="logo"
+            src={image}
+            width="40px"
+            height="auto"
+          />
+        </nav>
+      </div>
+      <div className="main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Bookkeeping" element={<Bookkeeping />} />
+          <Route path="/Mentoring" element={<Mentoring />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
