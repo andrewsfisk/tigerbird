b8eb4ddfd313b77835db07a4407a398fa88a21e4
andrewsfisk <andrewsfisk@gmail.com>
2023-05-14 21:15:08 +1000

    update

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -11,22 +11,30 @@ const Contact = () => {
       <hr></hr>
       <div className="logoFlex">
         <div>
-          <h5>Debbie King</h5>
+          <p className="title">Debbie King</p>
           <p>Bookkeeping and Account Manager</p>
-          <p>m: 0491 076 179</p>
-          <p>p: 03 9489 1790</p>
-          <p>e: accounts@tigerbirdinternational.com</p>
+          <p>
+            <a href="tel:0491 076 179">m: 0491 076 179</a>
+          </p>
+          <p>
+            <a href="tel:03 9489 1790">p: 03 9489 1790</a>
+          </p>
+          <p>
+            <a href="mailto:accounts@tigerbirdinternational.com">
+              e: accounts@tigerbirdinternational.com
+            </a>
+          </p>
           <p>
             Office 111 C/- Dallas Group Business Centre 135 Bamfield Road,
             Heidelberg Heights, Vic 3081.
           </p>
-          <p className="italic">
-            Debbie is a Certified Bookkeeper, Xero Advisor & Payroll Certified
-            with over 45 years accounting experience
-          </p>
         </div>
         <img className="img4" alt="phone" src={phone} />
       </div>
+      <p className="italic">
+        Debbie is a Certified Bookkeeper, Xero Advisor & Payroll Certified with
+        over 45 years accounting experience.
+      </p>
     </div>
   );
 };
